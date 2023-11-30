8cb66d7810fb047c2be5ce0c06d5da79e6afeec0
bobr <borya2072@gmail.com>
2023-11-30 20:09:04 +0200

    added swagger

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path, re_path
 
 from api import views
 
+from api.management.schema import schema_view
+
 url_list = [
     path('product-matched-list/', views.ProductListMatches.as_view(), name='product_matched_list'),
     path('product-list/', views.ProductList.as_view(), name='product_list'),
@@ -10,6 +12,8 @@ url_list = [
     re_path(r'dealer-detail/(?P<pk>\w+)/$', views.DealerDetail.as_view(), name='dealer_detail'),
     re_path(r'product-stat/(?P<pk>\w+)/$', views.ProductsStat.as_view(), name='product_stat'),
     re_path(r'operator-stat/(?P<pk>\w+)/$', views.OperatorStat.as_view(), name='operator_stat'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns = [
