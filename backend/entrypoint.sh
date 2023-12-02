556b32471bbd72e13755496186146383a538b16a
bobr <borya2072@gmail.com>
2023-12-02 19:24:21 +0200

    small fix entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,6 +6,8 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
+python manage.py loaddata test_data.json
+
 cp -r /app/collected_static/. /backend_static/static/
 
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
