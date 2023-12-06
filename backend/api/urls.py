db37c89a44200d6b8e6b2b0963eda568e85710d0
Хомутов Евгений <homutov@42dev.ru>
2023-12-06 14:09:02 +0500

    изменение структуры вьюх

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,20 +1,20 @@
 from django.urls import include, path, re_path
 
-from api import views
+from api.views import v1
 from api.management.schema import schema_view
 
 url_list = [
-    path('product-to-matched-list/', views.ProductListToMatches.as_view(), name='product_to_matched_list'),
-    path('product-list/', views.ProductList.as_view(), name='product_list'),
-    path('dealer-product-list/', views.DealerProductList.as_view(), name='dealer_product_list'),
-    path('product-matching/', views.ProductMatching.as_view(), name='product_matching'),
-    path('dealer-list/', views.DealerList.as_view(), name='dealer_list'),
-    path('dealers-stat/', views.DealersStat.as_view(), name='dealers_stat'),
-    path('match-stat/', views.MLStat.as_view(), name='ml_stat'),
-    re_path(r'dealer-detail/(?P<pk>\w+)/$', views.DealerDetail.as_view(), name='dealer_detail'),
-    re_path(r'product-stat/(?P<pk>\w+)/$', views.ProductsStat.as_view(), name='product_stat'),
-    path('ml-force-update/', views.MlForceUpdate.as_view(), name='ml_force_update'),
-    path('references/', views.AppDicts.as_view(), name='references'),
+    path('product-to-matched-list/', v1.ProductListToMatches.as_view(), name='product_to_matched_list'),
+    path('product-list/', v1.ProductList.as_view(), name='product_list'),
+    path('dealer-product-list/', v1.DealerProductList.as_view(), name='dealer_product_list'),
+    path('product-matching/', v1.ProductMatching.as_view(), name='product_matching'),
+    path('dealer-list/', v1.DealerList.as_view(), name='dealer_list'),
+    path('dealers-stat/', v1.DealersStat.as_view(), name='dealers_stat'),
+    path('match-stat/', v1.MLStat.as_view(), name='ml_stat'),
+    re_path(r'dealer-detail/(?P<pk>\w+)/$', v1.DealerDetail.as_view(), name='dealer_detail'),
+    re_path(r'product-stat/(?P<pk>\w+)/$', v1.ProductsStat.as_view(), name='product_stat'),
+    path('ml-force-update/', v1.MlForceUpdate.as_view(), name='ml_force_update'),
+    path('references/', v1.AppDicts.as_view(), name='references'),
     # TODO: Перенести в backend, сделать зависимым от debug=True
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
