d78093b4c518f5fd0b0e4aeafc1dbba8bab7c9b3
Хомутов Евгений <homutov@42dev.ru>
2023-12-08 02:45:17 +0500

    fix #

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,15 +4,15 @@ from api.views import v1
 from api.management.schema import schema_view
 
 url_list = [
-    path('product-to-matched-list/', v1.ProductListToMatches.as_view(), name='product_to_matched_list'), #
-    path('product-list/', v1.ProductList.as_view(), name='product_list'), #
-    path('dealer-product-list/', v1.DealerProductList.as_view(), name='dealer_product_list'), #
+    path('product-to-matched-list/', v1.ProductListToMatches.as_view(), name='product_to_matched_list'),
+    path('product-list/', v1.ProductList.as_view(), name='product_list'),
+    path('dealer-product-list/', v1.DealerProductList.as_view(), name='dealer_product_list'),
     path('product-matching/', v1.ProductMatching.as_view(), name='product_matching'),
-    path('dealer-list/', v1.DealerList.as_view(), name='dealer_list'), #
-    path('dealers-stat/', v1.DealersStat.as_view(), name='dealers_stat'), #
-    path('match-stat/', v1.MLStat.as_view(), name='ml_stat'), #
-    re_path(r'dealer-detail/(?P<pk>\w+)/$', v1.DealerDetail.as_view(), name='dealer_detail'), #
-    re_path(r'product-stat/(?P<pk>\w+)/$', v1.ProductsStat.as_view(), name='product_stat'), #
+    path('dealer-list/', v1.DealerList.as_view(), name='dealer_list'),
+    path('dealers-stat/', v1.DealersStat.as_view(), name='dealers_stat'),
+    path('match-stat/', v1.MLStat.as_view(), name='ml_stat'),
+    re_path(r'dealer-detail/(?P<pk>\w+)/$', v1.DealerDetail.as_view(), name='dealer_detail'),
+    re_path(r'product-stat/(?P<pk>\w+)/$', v1.ProductsStat.as_view(), name='product_stat'),
     path('ml-force-update/', v1.MlForceUpdate.as_view(), name='ml_force_update'),
     path('ml-force-update-product/', v1.MlForceUpdateProduct.as_view(), name='ml_force_product_update'),
     path('references/', v1.AppDicts.as_view(), name='references'),
